462696f76945b4382f1d7f7508e2bce9476ffdfc
QuangNV98 <quangoc98@gmail.com>
2021-02-25 16:52:34 +0700

    init project

---
+++ App.js
@@ -0,0 +1,183 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { StyleSheet, Text, View, Button, Easing } from "react-native"
+
+import { useIsFocused } from "@react-navigation/core";
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+const HomeStack = createStackNavigator()
+
+const HomeScreen = ({ navigation }) => {
+  // navigation.setOptions({
+  //   headerRight: () => (
+  //     <Button
+  //       title="Save"
+  //       onPress={() => {
+  //         //save the changes
+  //         navigation.replace("Home");
+  //       }}
+  //     />
+  //   )
+  // });
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>HomeScreen</Text>
+      <Button title="Go To Details Screen" onPress={() => navigation.navigate("Details")} />
+    </View>
+  )
+}
+
+const SettingsScreen = ({ navigation }) => {
+  const isFocused = useIsFocused();
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ color: isFocused ? "green" : "black" }}>SettingsScreen</Text>
+      <Button title="Go To Home Screen" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const FeedScreen = props => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>FeedScreen</Text>
+  </View>
+);
+
+const DetailsScreen = props => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>DetailsScreen</Text>
+  </View>
+);
+
+const HomeStackNavigator = ({ navigation, route }) => {
+  if (route.state) {
+    navigation.setOptions({
+      tabBarVisible: route.state.index > 0 ? false : true
+    });
+  }
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const HomeTabNavigator = ({ navigation, route }) => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name == "Home") {
+            iconName = "home";
+          } else if (route.name == "Feed") {
+            iconName = "filter";
+          } else if (route.name == "Settings") {
+            iconName = "settings";
+          }
+          return <Ionicons name={iconName} size={size} color={color}/>
+        }
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeStackNavigator} />
+      <Tab.Screen name="Feed" component={FeedScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  )
+}
+
+const config = {
+  animation: "spring",
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01
+  }
+};
+
+const closeConfig = {
+  animation: "timing",
+  config: {
+    duration: 500,
+    easing: Easing.linear
+  }
+};
+
+function getHeaderTitle(route) {
+  const routeName = route.state ? route.state.routes[route.state.index].name : "Home";
+
+  switch (routeName) {
+    case "Home":
+      return "Home";
+    case "Feed":
+      return "Feed";
+    case "Settings":
+      return "Settings";
+  }
+}
+
+function shouldHeaderBeShown(route) {
+  const routeName = route.state ? route.state.routes[route.state.index].name : "Home";
+  switch (routeName) {
+    case "Home":
+      return false;
+  }
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+          // transitionSpec: {
+          //   open: config,
+          //   close: closeConfig
+          // }
+        }}
+        headerMode="float"
+        animation="fade"
+      >
+        <Stack.Screen
+          options={({ route }) => ({
+            title: getHeaderTitle(route),
+            headerShown: shouldHeaderBeShown(route)
+          })}
+          name="Home"
+          component={HomeTabNavigator}
+        />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
